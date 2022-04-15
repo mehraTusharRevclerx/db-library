9eb1cbb6bbe0b1ad4f0f560de9e49321bc562823
mehraTusharRevclerx <100279723+mehraTusharRevclerx@users.noreply.github.com>
2022-04-15 18:09:23 +0530

    Update db.js

--- js/db.js
+++ js/db.js
@@ -1,47 +1,45 @@
-"use strict"
+"use strict";
 //! insert()____remove()____update()_____view()_____get()________first()________
 class Db {
-    constructor(arr) {
-      this.arr = arr
+  constructor(arr) {
+    this.arr = arr;
+  }
+
+  insert = (newObj) => {
+    if (typeof newObj === "object") {
+      this.arr.push(newObj);
+      return this;
     }
+  };
 
-    insert = (newObj) => {
-      if(typeof newObj === "object"){
-        this.arr.push(newObj); 
-        return this
-      }
-    };
-
-    get = (objKey) =>{
-        for (let i = 0; i < this.arr.length; i++) {
-          Object.values(this.arr[i]).find(e =>{
-            if(e == objKey){
-             //  console.log(Object.values(this.arr[i]))
-              return (this.arr[i]); 
-            }
-           });    
+  find = (objKey) => {
+    for (let i = 0; i < this.arr.length; i++) {
+      Object.values(this.arr[i]).find((e) => {
+        if (e == objKey) {
+          console.log(this.arr[i]);
+          // return this.arr[i];
         }
-      }
-    
-      first = () =>{
-        // console.log(this.arr)
-        // if(this.arr.length !== 0 ){
-        //   return this.arr[0]
-        // }
-          return (this.arr.length !== 0 &&  this.arr[0]);
-      }
+      });
+    }
+  };
 
-      last = () =>{
-        return (this.arr.length !== 0 &&  this.arr[this.arr.length-1]);
+  first = () => {
+    // console.log(this.arr)
+    // if(this.arr.length !== 0 ){
+    //   return this.arr[0]
+    // }
+    return this.arr.length !== 0 && this.arr[0];
+  };
 
-      }
+  last = () => {
+    return this.arr.length !== 0 && this.arr[this.arr.length - 1];
+  };
 
-      remove = () =>{
-        // console.log(this.arr)  
-        //   while (this.arr.length) { 
-        //     this.arr.pop(); 
-        // }
-        return this.arr.length !== 0 &&  (this.arr.length = 0) ;        
-      }
+  remove = () => {
+    // console.log(this.arr)
+    //   while (this.arr.length) {
+    //     this.arr.pop();
+    // }
+    return this.arr.length !== 0 && (this.arr.length = 0);
+  };
 }
-  
