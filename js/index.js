935a5dbac3fe8f6023bcb24e6fb8ad4b52818514
mehraTusharRevclerx <100279723+mehraTusharRevclerx@users.noreply.github.com>
2022-04-15 18:11:33 +0530

    linked all buttons to their methods
    created event listeners for all the buttons and linked them to the respective methods and call all the methods inside them.

--- js/index.js
+++ js/index.js
@@ -1,35 +1,86 @@
-"use strict"
+"use strict";
+const dbBtn = document.querySelector("#dbBtn");
+const insertBtn = document.querySelector("#insertBtn");
+const firstBtn = document.querySelector("#firstBtn");
+const lastBtn = document.querySelector("#lastBtn");
+const removeBtn = document.querySelector("#removeBtn");
+const findBtn = document.querySelector("#findBtn");
 
 async function fetchJson() {
-        const response = await fetch("/assets/people.json");
-        if (!response.ok) {
-          const message = `An error has occured: ${response.status}`;
-          throw new Error(message);
-        }
-        const people = await response.json()
-        console.log(people);
+  const response = await fetch("/assets/people.json");
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
 
-        const db = new Db(people)
-        console.log(db)
+  const people = await response.json();
+  console.log(people);
 
-        db.insert(newObj2);
-        
-        // const remove = db.remove()
-        // console.log(remove)
-        
-        
-        const first = db.first("arun")
-        console.log(first)
-        
-        const last = db.last()
-        console.log(last)
-        
-        const firstname = db.get("arun")
-        console.log(firstname)
-      }
-      fetchJson().catch(error => {
-        error.message;
-      });
+  let db;
 
-let newObj = [{firstName: 'Jgaoe', lastName: 'Jacksgaon', gender: 'male', age: 28, number: '7334282382'},{firstName: 'Jgae', lastName: 'Jcsgaon', gender: 'male', age: 23, number: '7334285382'}]
-let newObj2 = {firstName: 'gea', lastName: 'Jack', gender: 'male', age: 22, number: '73322582382'};
+  dbBtn.addEventListener("click", () => {
+    db = new Db(people);
+    console.log(db.arr);
+    yourDb.textContent = JSON.stringify(db.arr, null, null);
+  });
+
+  insertBtn.addEventListener("click", () => {
+    db.insert(newObj2);
+    yourDb.textContent = JSON.stringify(db.arr, null, null);
+  });
+
+  firstBtn.addEventListener("click", () => {
+    const firstRes = db.first();
+    yourDb.textContent = JSON.stringify(firstRes);
+    console.log(firstRes);
+  });
+
+  lastBtn.addEventListener("click", () => {
+    const lastRes = db.last();
+    yourDb.textContent = JSON.stringify(lastRes);
+    // console.log(last);
+  });
+
+  removeBtn.addEventListener("click", () => {
+    const rem = db.remove();
+    yourDb.textContent = `your database has ${rem} entries`;
+    // console.log(last);
+  });
+
+  findBtn.addEventListener("click", () => {
+    const firstname = db.find("parul");
+    yourDb.textContent = JSON.stringify(firstname);
+    console.log(firstname);
+  });
+
+  console.log(firstname);
+  // const remove = db.remove()
+  // console.log(remove)
+}
+fetchJson().catch((error) => {
+  error.message;
+});
+
+let newObj = [
+  {
+    firstName: "Jgaoe",
+    lastName: "Jacksgaon",
+    gender: "male",
+    age: 28,
+    number: "7334282382",
+  },
+  {
+    firstName: "Jgae",
+    lastName: "Jcsgaon",
+    gender: "male",
+    age: 23,
+    number: "7334285382",
+  },
+];
+let newObj2 = {
+  firstName: "gea",
+  lastName: "Jack",
+  gender: "male",
+  age: 22,
+  number: "73322582382",
+};
